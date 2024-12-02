8e82c8622ee3e8d48442d2c2fcd6f576f7722a91
Ahsan280 <ahsanrehman281@gmail.com>
2024-12-02 13:01:47 +0500

    PNG

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assets: {
-    // Include all assets in the "assets" folder
-    include: ["**/*.png/*.PNG"],
+    include: ["**/*.png", "**/*.PNG"],
   },
 });
